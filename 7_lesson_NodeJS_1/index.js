13c415b9cf3326305fe54abb714c1b90462929fe
morozovDm <44220292+morozovDm@users.noreply.github.com>
2018-11-11 18:15:24 +0400

    Update index.js

--- 7_lesson_NodeJS_1/index.js
+++ 7_lesson_NodeJS_1/index.js
@@ -1,5 +1,5 @@
 const calculator = require("./calculator");
-var operandA = process.argv[3], operandB = process.argv[5], operation = process.argv[4];
+var operandA = process.argv[2], operandB = process.argv[4], operation = process.argv[3];
 try {
 console.log(calculator.calculate(operandA, operation, operandB));
 } catch (err) {
