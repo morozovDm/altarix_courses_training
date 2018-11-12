275bc9477e81782417413701ccc8ebfaf2944524
morozovDm <44220292+morozovDm@users.noreply.github.com>
2018-11-12 07:15:40 +0300

    Update calculator.js

--- 7_lesson_NodeJS_1/calculator.js
+++ 7_lesson_NodeJS_1/calculator.js
@@ -1,7 +1,7 @@
 function calculate(operand_1, operator, operand_2){
     console.log(operand_1, operator, operand_2)
     if(typeof operand_1 !== "number" || typeof operand_2 !== "number") 
-      throw "one of operands is not a number";
+      throw new Error("one of operands is not a number");
     switch(operator) {
       case '+':
         return operand_1 + operand_2;
@@ -12,7 +12,7 @@ function calculate(operand_1, operator, operand_2){
       case '/':
         return operand_1 / operand_2;
       default:
-        throw "sorry this operator not support";
+        throw new Error("sorry this operator not support");
     } 
   }
   exports.calculate = calculate;
