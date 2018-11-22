a1e1a67bfd81786b3088da11fd61faba3b8f7f0d
morozovDm <44220292+morozovDm@users.noreply.github.com>
2018-11-22 15:52:15 +0300

    add classNames

--- 9_lesson_React_Chat/src/components/Message.js
+++ 9_lesson_React_Chat/src/components/Message.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import ClassNames from 'classnames'
 import icon from '../resources/anonymus.svg'
 class Message extends Component {
   render() {
+      var msg = ClassNames({
+        message: true,
+        right: this.props.user === this.props.message.username,
+        left: !this.props.user === this.props.message.username
+    });
     return (
-      <li className={`message ${this.props.user === this.props.message.username ? "right" : "left"}`}>
-        <div class='msg-sender'>
-          <img src={this.props.message.img ? this.props.message.img : icon} alt='anonymous'></img>
-          <div>{this.props.message.username}</div>
-        </div>
-        <div class='msg-content'>
-          {this.props.message.content}
-        </div>
-      </li>
+    <li className = {msg}>
+    <div class='msg-sender'>
+      <img src={this.props.message.img ? this.props.message.img : icon} alt='anonymous'></img>
+      <div>{this.props.message.username}</div>
+    </div>
+    <div class='msg-content'>
+      {this.props.message.content}
+    </div>
+      </li >
     );
   }
 }
 
 export default Message;
+
+
